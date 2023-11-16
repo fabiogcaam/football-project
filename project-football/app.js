99ad4b0f9ddb55fc9f84e5cc26fea48779964da1
Mohamed khamkham <mohamkham1999@gmail.com>
2023-11-16 15:41:55 +0100

    process

--- project-football/app.js
+++ project-football/app.js
@@ -7,14 +7,9 @@ const app = express()
 require("./config/index")(app)
 require("./config/session.config")(app)
 
-<<<<<<< HEAD
-const capitalize = require("./utils/capitalize")
-const projectName = "project-football"
-=======
-// default value for title local
+
 const capitalize = require("./utils/capitalize");
 const projectName = "Iron Football";
->>>>>>> 90b1b996d708a443a5fc301257a752fc3b9fe167
 
 app.locals.appTitle = `${capitalize(projectName)}`
 
