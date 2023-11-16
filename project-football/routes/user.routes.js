a912917ecc6ad79b2302d8f5e63013727d1a6734
Mohamed khamkham <mohamkham1999@gmail.com>
2023-11-16 12:17:03 +0100

    finish button edit-delete

--- project-football/routes/user.routes.js
+++ project-football/routes/user.routes.js
@@ -1,17 +1,22 @@
 const router = require("express").Router()
-const { isLoggedIn, checkRole } = require('../middleware/guard-route')
+const { isLoggedIn, checkRole, isOwner } = require('../middleware/guard-route')
 const User = require('../models/User.model')
 
 
 
 router.get('/profile', isLoggedIn, (req, res) => {
 
-    res.render('user/detail-user', { user: req.session.currentUser })
+    res.render('user/detail-user', {
+        user: req.session.currentUser,
+        isAdminOrOwner: req.session.currentUser.role,
+        isAdmin: req.session.currentUser.role === 'ADMIN'
+    })
 
 })
-// router.get("/admin", isLoggedIn, checkRole('ADMIN'), (req, res) => {
-//     res.render("user/   ", { user: req.session.currentUser })
-// })
+
+router.get("/admin", isLoggedIn, checkRole('ADMIN'), (req, res) => {
+    res.render("user/admin", { user: req.session.currentUser })
+})
 
 
 router.post('/addPlayer/:idPlayer', isLoggedIn, (req, res, next) => {
@@ -35,7 +40,7 @@ router.get('/:id/edit', isLoggedIn, isOwner, (req, res, next) => {
 
     User
         .findById(id)
-        .then(user => res.render('user/edit-user', user))
+        .then(user => res.render('user/edit-user', { user: req.session.currentUser }))
         .catch(err => next(err))
 
 
@@ -43,11 +48,27 @@ router.get('/:id/edit', isLoggedIn, isOwner, (req, res, next) => {
 
 router.post('/:id/edit', isLoggedIn, isOwner, (req, res, next) => {
     const { username, email, avatar, description } = req.body
+    req.session.currentUser.username = username
+    req.session.currentUser.email = email
+    req.session.currentUser.avatar = avatar
+    req.session.currentUser.description = description
     const { id } = req.params
 
     User
         .findByIdAndUpdate(id, { username, email, avatar, description })
         .then(() => res.redirect(`/user/profile`))
+        .catch(err => console.log(err))
+})
+
+//Delete
+
+router.post('/:id', isLoggedIn, (req, res) => {
+
+    const { id } = req.params
+
+    User
+        .findByIdAndDelete(id)
+        .then(() => res.redirect('/'))
         .catch(err => console.log(err))
 })
 
